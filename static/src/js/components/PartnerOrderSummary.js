d5642a308de95ad4b8507cd162cc0f2e45f87dbf
Kyle Sin Lynn <kylesinlynn@gmail.com>
2022-12-09 11:35:16 +0630

    feat: mount component to class

--- static/src/js/components/PartnerOrderSummary.js
+++ static/src/js/components/PartnerOrderSummary.js
@@ -1,5 +1,8 @@
 odoo.define('owl_odoo_testing.PartnerOrderSummary', function (require) {
+   
+    const FormRenderer = require('web.FormRenderer')
     const { Component } = owl;
+    const { ComponentWrapper } = require('web.OwlCompatibility')
 
     class PartnerOrderSummary extends Component {
 
@@ -7,5 +10,15 @@ odoo.define('owl_odoo_testing.PartnerOrderSummary', function (require) {
 
     Object.assign(PartnerOrderSummary, {
         template: 'owl_odoo_testing.PartnerOrderSummary'
+    })
+
+    FormRenderer.include({
+        async _render() {
+            await this._super(...arguments)
+
+            for(const element of this.el.querySelectorAll('.o_partner_order_summary')) {
+                (new ComponentWrapper(this, PartnerOrderSummary)).mount(element)
+            }
+        }
     })
 })
