cc025534e9497c3c6773e83eb163b54343ef9859
Kyle Sin Lynn <kylesinlynn@gmail.com>
2022-12-09 10:38:12 +0630

    feat: create __manifest__.py for odoo module

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Odoo OWL Testing',
+    'summary': 'Example for OWL',
+    'description': 'OWL Odoo Integration',
+    'author': 'Kyle Sin Lynn',
+    'website': 'http://kylesinlynn.ml',
+    'version': '0.0.1',
+    'depends': ['base'],
+    'demo': [],
+    'data': []
+}
